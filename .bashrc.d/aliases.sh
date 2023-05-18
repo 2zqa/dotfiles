aa779f98ecfa834c37c3d26df24d72340e127efe
2zqa <25235249+2zqa@users.noreply.github.com>
2023-05-18 15:09:09 +0200

    Remove .zshrc and old scripts, add .bashrc.d files

---
+++ .bashrc.d/aliases.sh
@@ -0,0 +1,4 @@
+alias vim='nvim'
+alias ls='ls --color --group-directories-first'
+alias wl-copy='wl-copy --trim-newline'
+alias ip='ip -c'
---
+++ .bashrc.d/exports.sh
@@ -0,0 +1,3 @@
+# Editor
+export EDITOR=/usr/bin/nvim
+export VISUAL=$EDITOR
